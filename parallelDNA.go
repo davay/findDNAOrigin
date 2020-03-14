083687c03bcee83570d14069a46c5c64be26c21b
Devin Lim <davay@devinl.im>
2020-03-13 18:02:23 -0700

    init

---
+++ parallelDNA.go
@@ -0,0 +1,124 @@
+package main
+
+const NumThreads = 16
+
+type TallyType struct {
+	c int
+	g int
+}
+
+func parent(i int) int {
+	return (i - 1) / 2
+}
+
+func left(i int) int {
+	return i*2 + 1
+}
+
+func right(i int) int {
+	return left(i) + 1
+}
+
+func isLeaf(i int, size int) bool {
+	return right(i) >= size*2-1
+}
+
+func calcSum(i int, level int, output []TallyType, size int) {
+
+	if !isLeaf(i, size) {
+		calcSum(left(i), level+1, output, size)
+		calcSum(right(i), level+1, output, size)
+		output[i].c = output[left(i)].c + output[right(i)].c
+		output[i].g = output[left(i)].g + output[right(i)].g
+	}
+}
+
+func calcPrefix(i int, sumPrior TallyType, level int, input []TallyType, output []TallyType, size int) {
+	if isLeaf(i, size) {
+		output[i-size+1].c = sumPrior.c + input[i].c
+		output[i-size+1].g = sumPrior.g + input[i].g
+	} else {
+		calcPrefix(left(i), sumPrior, level+1, input, output, size)
+		preSumPrior := TallyType{sumPrior.c + input[left(i)].c, sumPrior.g + input[left(i)].g}
+		calcPrefix(right(i), preSumPrior, level+1, input, output, size)
+	}
+}
+
+func fixInput(input string) string {
+
+	power2 := 1
+	for i := 1; i < len(input); i *= 2 {
+		power2 = i
+	}
+	power2 *= 2
+	println(power2)
+	output := input
+
+	for i := len(input); i < power2; i++ {
+		output += "X"
+	}
+	return output
+}
+
+// func printData(input []TallyType, size int) {
+// 	for i := size - 1; i < size*2-1; i++ {
+// 		print("[", input[i].c, ", ", input[i].g, "], ")
+
+// 	}
+// 	println()
+// 	print("--------------------------------------------------")
+// 	println()
+// 	for i := size - 1; i < size*2-1; i++ {
+// 		print("[ ", i, " ], ")
+// 	}
+// }
+
+func printData(input []TallyType, size int) {
+	for i := 0; i < size; i++ {
+		print("[", input[i].c, ", ", input[i].g, "], ")
+
+	}
+	println()
+	print("--------------------------------------------------")
+	println()
+	for i := 0; i < size; i++ {
+		print("[  ", i, " ], ")
+	}
+}
+
+func main() {
+	input :=
+
+		"aactctatacctcctttttgtcgaatttgtgtgatttatagagaaaatcttattaact" +
+			"gaaactaaaatggtaggtttggtggtaggttttgtgtacattttgtagtatctgatttttaattacat" +
+			"accgtatattgtattaaattgacgaacaattgcatggaattgaatatatgcaaaacaaacctaccacc" +
+			"aaactctgtattgaccattttaggacaacttcagggtggtaggtttctgaagctctcatcaatagact" +
+			"attttagtctttacaaacaatattaccgttcagattcaagattctacaacgctgttttaatgggcgtt" +
+			"gcagaaaacttaccacctaaaatccagtat"
+	input = fixInput(input)
+
+	size := len(input)
+	data := make([]TallyType, size*2-1)
+	for pos, char := range input {
+		y := TallyType{0, 0}
+		if char == 'c' {
+			y.c = 1
+		}
+		if char == 'g' {
+			y.g = 1
+		}
+		data[pos+size-1] = y
+	}
+
+	x := TallyType{0, 0}
+	outputArr := make([]TallyType, size)
+	println("BEFORE CALCSUM")
+	printData(data, size*2-1)
+	calcSum(0, 0, data, size)
+	println("BEFORE CALCPREFIX")
+	calcPrefix(0, x, 0, data, outputArr, size)
+	printData(data, size*2-1)
+	println("AFTER CALCPREFIX")
+	printData(outputArr, size)
+
+}
